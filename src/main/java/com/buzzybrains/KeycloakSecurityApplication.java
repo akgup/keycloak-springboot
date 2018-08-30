8d1bdd2f63fafee60e1d145b83dcd92ca054e986
Akshay Gupta <akshay@buzzybrains.com>
2018-08-30 15:41:50 +0530

    Update KeycloakSecurityApplication.java

--- src/main/java/com/buzzybrains/KeycloakSecurityApplication.java
+++ src/main/java/com/buzzybrains/KeycloakSecurityApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KeycloakSecurityApplication {
 
-	//adding test comment
 	public static void main(String[] args) {
 		SpringApplication.run(KeycloakSecurityApplication.class, args);
 	}
